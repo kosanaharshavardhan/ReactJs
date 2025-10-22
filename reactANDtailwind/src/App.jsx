f24cbccd48f7df3308a106d2bf074cf67113d435
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-10-22 23:39:34 +0530

    some update

--- reactANDtailwind/src/App.jsx
+++ reactANDtailwind/src/App.jsx
@@ -1,7 +1,18 @@
 // import './App.css'
 const App=()=>{
+
+  function printevent(ee){
+    console.log(":( --> :)")
+    console.log(ee)
+  }
+
+
   return <>
-    <div class="border-amber-600  border-2 h-full inline-block">App</div>
+    {/* <div class="border-amber-600  border-2 h-full inline-block">App</div>
+     */}
+     <button onClick={function(e){
+      printevent(e)
+     }}>ClickMe</button>
   </>
 }
 
